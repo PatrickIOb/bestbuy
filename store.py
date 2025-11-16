87c6dfec92e77101d4b9721b658a8d3db4075893
PatrickIOb <patrick.bauer95@gmx.net>
2025-11-16 18:50:32 +0100

    Store Class added

---
+++ store.py
@@ -0,0 +1,60 @@
+import products
+from products import Product
+
+
+class Store:
+
+    def __init__(self, products):
+        # list of Product objects
+        self.products = list(products)
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        total = 0
+        for product in self.products:
+            total += product.get_quantity()
+        return total
+
+    def get_all_products(self):
+        # return only active products
+        return [prod for prod in self.products if prod.is_active()]
+
+    def order(self, shopping_list) -> float:
+        """
+        shopping_list is a list of tuples: (Product, quantity)
+        Buy each product, sum up total price
+        """
+        total_price = 0
+
+        # First validate the entire order BEFORE buying anything
+        for product, quantity in shopping_list:
+            if quantity > product.get_quantity():
+                raise ValueError(
+                    f"Not enough quantity for {product.name}"
+                )
+
+        # If all good, perform the buys
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+
+        return total_price
+
+def main():
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+
+    best_buy = Store(product_list)
+    products = best_buy.get_all_products()
+    print(best_buy.get_total_quantity())
+    print(best_buy.order([(products[0], 1), (products[1], 2)]))
+
+
+if __name__ == "__main__":
+    main()
